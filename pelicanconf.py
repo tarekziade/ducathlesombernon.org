91cebf33a5b738b8a5b478941fe39592aef8ff13
Tarek Ziade <tarek@ziade.org>
2021-11-06 15:07:45 +0100

    remove empty pages for now

--- pelicanconf.py
+++ pelicanconf.py
@@ -37,7 +37,7 @@ TAG_SAVE_AS = 'tag/{slug}/index.html'
 
 MENUITEMS = [('Présentation', '/pages/presentation.html'),
              ('Entrainements', '/pages/entrainements.html'),
-             ('Association', '/pages/association.html'))
+             ('Association', '/pages/association.html')]
 
              #('Événements', '/pages/evenements.html'),
              #('Infos pratiques', '/pages/informations-pratiques.html')]
